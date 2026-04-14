76ea605a8785f2e0b5b1f503196271a32bf118e1
Ezhil0123 <150767920+Ezhil0123@users.noreply.github.com>
2026-04-14 11:50:23 +0530

    Time: 5 ms (83.35%) | Memory: 46.6 MB (92.22%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i ++) {
+            graph.add(new ArrayList<>());
+        }
+        int[] indegree = new int[numCourses];
+        for (int[] pre : prerequisites) {
+            int course = pre[0];
+            int prereq = pre[1];
+            graph.get(prereq).add(course);
+            indegree[course]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i ++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        int[] result = new int[numCourses];
+        int index = 0;
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            result[index++] = curr;
+            for (int neighbor : graph.get(curr)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        if (index == numCourses) {
+            return result;
+        }
+        return new int[0];
+    }
+}
